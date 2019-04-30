fe4ed2d97791a52ca8c69589ad942e436df00024
Mikayla Chin <MikaylaChin@users.noreply.github.com>
2019-04-30 14:28:25 -0700

    update 5

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+	createCanvas(windowWidth,windowHeight);
+	noCursor();
+}
+
+function draw(){
+	ellipse(mouseX,mouseY,100,100);
+}
+
+function mousePressed(){
+
+}
